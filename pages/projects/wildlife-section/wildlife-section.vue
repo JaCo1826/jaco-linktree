9fa4dbbad4f331cba3fc5dd393a7c763fd572be9
JaCo1826 <jaco1766@gmail.com>
2026-06-30 17:15:34 +0200

    Updated Section and added structure for Content

--- pages/projects/wildlife-section/wildlife-section.vue
+++ pages/projects/wildlife-section/wildlife-section.vue
@@ -2,6 +2,28 @@
 
 <template>
   <h1>NOT READY</h1>
+
+  <main class="wildlife-page">
+    <h1>Wildlife Portfolio</h1>
+
+    <AppDivider />
+
+    <div class="wildlife-collage">
+      <div class="section-1">
+        <h2>WIP</h2>
+        <!-- <img src="/.webp" alt="" />
+        <img src="/.webp" alt="" class="wideAndHeight" />
+        <img src="/.webp" alt="" class="height" />
+        <img src="/.webp" alt="" />
+        <img src="/.webp" alt="" />
+        <img src="/.webp" alt="" />
+        <img src="/.webp" alt="" class="wide" />
+        <img src="/.webp" alt="" /> -->
+      </div>
+    </div>
+  </main>
+
+  <AppFooter />
 </template>
 
 <style lang="scss" scoped src="./wildlife-section.scss"></style>
