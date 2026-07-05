28e7b81f4e6c6d0b1fad7260328b335503ce0db8
JaCo1826 <jaco1766@gmail.com>
2026-07-05 16:36:43 +0200

    fixed typo

--- pages/projects/travel/norwegen.vue
+++ pages/projects/travel/norwegen.vue
@@ -13,7 +13,7 @@
     <PullQuote>"Das Licht hier kennt keine Eile."</PullQuote>
     <p>
       Der Urlaub war wahrlich mehr als eine Expedition. In Norwegen war es möglich Energie zu tanken
-      und zu stauenen. Unberührte Landschaften, nirgend Gebäude oder Anzeichen davon. Einfach nur
+      und zu staunen. Unberührte Landschaften, nirgends Gebäude oder Anzeichen davon. Einfach nur
       die Gelassenheit der Natur und ihr raue Brutalität. Highlight war hier der Galdhøppigen.
 
       <br />
