4248136bf6c421baf6c51fdfe98a704097d58af1
JaCo1826 <jaco1766@gmail.com>
2026-08-08 16:31:11 +0200

    updated Impressum with location and disclaimer

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,14 +1,34 @@
+<script setup lang="ts"></script>
 <template>
   <h1>Impressum</h1>
 
   <p>
     Colin Noel Held<br />
+    Von Bodelschwingh Str. 10<br />
     Soltau, Deutschland<br />
-    Photographer
+    Photographer / Videographer
   </p>
 
   <h2>Kontakt</h2>
+  <p>Telefon: +49 (0) 174 6408706</p>
   <p>E-Mail: jaco1766@proton.me</p>
 
-  <p>Quelle: <a href="https://www.e-recht24.de">https://www.e-recht24.de</a></p>
+  <h2>Verantwortlich für den Inhalt</h2>
+  <p>
+    Colin Noel Held<br />
+    Von Bodelschwingh Str. 10<br />
+    Soltau, Deutschland<br />
+  </p>
+
+  <h2>Verbraucherstreitbeilegung</h2>
+  <p>
+    Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer<br />
+    Verbraucherschlichtungsstelle teilzunehmen.
+  </p>
+
+  <h2>Urheberrecht</h2>
+  <p>
+    Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem<br />
+    deutschen Urheberrecht. Beiträge Dritter sind als solche gekennzeichnet.
+  </p>
 </template>
